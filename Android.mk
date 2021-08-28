da8223ea7dcc850a774496af16fe22db0f1b3de8
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-08-28 17:18:27 +0200

    Initial a52q tree
    Change-Id: I7f1c50bda155154412ad0a69ec9bf4c156414f09

---
+++ Android.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),a52q)
+
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+endif
