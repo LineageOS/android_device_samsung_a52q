dee1a84f1763a2878cedbd64aa31637d232a8400
LuK1337 <priv.luk@gmail.com>
2024-08-28 15:32:59 +0200

    a52q: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- device.mk
+++ device.mk
@@ -29,3 +29,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Camera
+$(call soong_config_set,samsungCameraVars,extra_ids,54)
